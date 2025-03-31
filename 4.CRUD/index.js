5805cd38f9d499754e0177703e269403ce4fc0c8
prabeenragupathi <prabeenprabu@gmail.com>
2025-04-01 00:20:07 +0530

    learnt how to create schema, insertion & fetching

--- 4.CRUD/index.js
+++ 4.CRUD/index.js
@@ -4,3 +4,44 @@ mongoose
   .connect("mongodb://localhost:27017/practice")
   .then(() => console.log("Connected to DB"))
   .catch((err) => console.log(err));
+
+//creating schema
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+
+//? creating model in db
+const Course = mongoose.model("Course", courseSchema);
+
+//creating inserting data in db
+const insertData = (name, author, tags, isPublished) => {
+  const course = new Course({
+    name,
+    author,
+    tags,
+    isPublished,
+  });
+  course.save();
+  console.log("added course" + course);
+};
+
+// insertData("node js", "prabeen ragupathi", ["js, node"], true);
+// insertData("express js", "preethi ragupathi", ["js, node", "express"], true);
+// insertData(
+//   "react js",
+//   "prabeen ragupathi",
+//   ["js", "react", "tailwindcss"],
+//   true
+// );
+// insertData("tailwind css", "prabeen ragupathi", ["css", "tailwindcss"], false);
+
+const allCourses = async () => {
+  const all = await Course.find().countDocuments();
+  console.log(all);
+};
+
+allCourses();
